782912613918e26ed476f04681742b66fa1a2533
Matheus G. Dias <matheusgiga123@gmail.com>
2026-01-17 19:11:37 -0300

    feat(factorial): create a function that calculate the factorial

--- MyModule.scala
+++ MyModule.scala
@@ -5,12 +5,22 @@ object MyModule {
     def abs(n: Int): Int =
         if (n < 0) -n
         else n
+
+    def factorial(n: Int): Int = {
+        def go(n: Int, acc: Int): Int = 
+            if (n <= 0) acc
+            else go(n-1, n*acc)
+
+        go(n, 1)
+    }
         
     private def formatAbs(x: Int) = {
         val msg = "The absolute value of %d is %d"
         msg.format(x, abs(x))
     }
     
-    def main(args: Array[String]): Unit =
+    def main(args: Array[String]): Unit = {
         println(formatAbs(-42))
+        println(factorial(7))
+    }
 }
