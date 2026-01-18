bf7c370aa38a43499478347458a26717733d3959
Matheus G. Dias <matheusgiga123@gmail.com>
2026-01-18 16:54:01 -0300

    feat: create a HOFs (High order functions)
    - Create a high order function to print the result

--- MyModule.scala
+++ MyModule.scala
@@ -18,9 +18,19 @@ object MyModule {
         val msg = "The absolute value of %d is %d"
         msg.format(x, abs(x))
     }
+
+    private def formatFactorial(n: Int) = {
+        val msg = "The factorial of %d is %d."
+        msg.format(n, factorial(n))
+    }
+
+    def formatResult(name: String, n: Int, f: Int => Int) = {
+        val msg = "The %s of %d is %d."
+        msg.format(name, n, f(n))
+    }
     
     def main(args: Array[String]): Unit = {
-        println(formatAbs(-42))
-        println(factorial(7))
+        println(formatResult("absolute value", -42, abs))
+        println(formatFactorial(7))
     }
 }
